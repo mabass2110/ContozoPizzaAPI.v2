42a3d459a06c75bf24e773e07415d2db08038985
helder mabasso <hfmabasso@gmail.com>
2024-07-20 22:17:16 +0200

    GetAll method that return all pizzas in Pizzas List

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,27 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    // GET all action
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() =>
+    PizzaService.GetAll();
+
+    // GET by Id action
+
+    // POST action
+
+    // PUT action
+
+    // DELETE action
+}
